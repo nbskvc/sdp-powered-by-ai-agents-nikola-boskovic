3bd9cab833c880f0c19355001eed04f3efa6655c
Nikola Boskovic <nikola.boskovic.2003@gmail.com>
2026-04-19 14:03:04 +0200

    #1 test(grid): GRID-BE-001.1-S2 valid grid value satisfies Grid type

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -9,3 +9,15 @@ def test_grid_be_001_1_s1_grid_type_alias_is_importable():
 
     # THEN it resolves without error and is usable
     assert Grid is not None  # nosec B101
+
+
+# GRID-BE-001.1-S2: A valid grid value satisfies the type
+def test_grid_be_001_1_s2_valid_grid_value_satisfies_type():
+    # GIVEN a set of coordinate tuples
+    from grid import Grid
+
+    # WHEN assigned to a Grid-annotated variable
+    g: Grid = {(0, 0), (1, 2)}
+
+    # THEN it is usable as a grid
+    assert g == {(0, 0), (1, 2)}  # nosec B101
